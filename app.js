a6b5b499eeb9840a85fd36e3313f36e95d8118ee
Akhil Boddu <akhil1234mara@gmail.com>
2023-07-06 19:42:26 +0300

    CRUD done with vanilla JS

--- app.js
+++ app.js
@@ -1,36 +1,23 @@
-const outputPostStatus = (post) => {
-  const output = `This post is ${
-    post.isPublic ? "public" : "private"
-  }, posted by ${post.username}, and currently has ${post.likes} likes and ${
-    post.comments.length
-  } comments.`;
-  return output;
-};
-
-const post = {
-  username: "akhilboddu",
-  imageLink:
-    "https://images.unsplash.com/photo-1581093458791-3b1c7e0c9d7d?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  caption: "This is a post",
-  likes: 0,
-  comments: [],
-  shares: 0,
-  isPublic: true,
-  createdAt: "2020-02-07T17:00:00.000Z",
-};
-
-// Spread operator
-// Array of objects - feed
-const feed = [
-  post,
-  { ...post, username: "johndoe" },
-  { ...post, username: "janedoe" },
-  { ...post, username: "elonmusk" },
-];
-
+var feed = []; // global property
 console.log("feed", feed);
 
 // CRUD - Create, Read, Update, Delete
+// CREATING A POST
+const createPost = (imageLink, caption, username) => {
+  const newPost = {
+    id: feed.length,
+    username: username,
+    imageLink: imageLink,
+    caption: caption,
+    likes: 0,
+    comments: [],
+    shares: 0,
+    isPublic: true,
+    createdAt: new Date(),
+  };
+  feed.push(newPost);
+  // outputFeed(feed);
+};
 
 // READING ALL POSTS
 // array + anonyomous function: map
@@ -39,9 +26,79 @@ const outputFeed = (feed) => {
   // map function returns a new array of just the statuses
   const output = feed.map((p) => {
     console.log(outputPostStatus(p));
-    return outputPostStatus(p);
+    return p;
+    // return outputPostStatus(p);
   });
-  // console.log(output);
+  console.log("NEW MAPPED ARRAY: ", output);
 };
 
-outputFeed(feed);
+// UPDATE POST
+const updatePost = (id, newImageLink, newCaption) => {
+  // update the post with the specific id
+
+  const updatedFeed = feed.map((post) => {
+    if (post.id === id) {
+      post.imageLink = newImageLink;
+      post.caption = newCaption;
+    }
+    return post;
+  });
+  feed = updatedFeed;
+};
+
+// DELETE POST
+const deletePost = (id) => {
+  // delete the post with the specific id
+  const updatedFeed = feed.filter((post) => {
+    if (post.id !== id) {
+      return post;
+    }
+  });
+  feed = updatedFeed;
+  outputFeed(feed);
+};
+
+const outputPostStatus = (post) => {
+  const output = `
+  POST INFO:
+  ID: ${post.id}
+  Username: ${post.username}
+  Image Link: ${post.imageLink}
+  Caption: ${post.caption}
+  `;
+  return output;
+};
+
+// IMPLEMENTATION
+
+// outputFeed(feed);
+// demo of creating a post
+createPost(
+  "https://frabjous-pavlova-ebea4c.netlify.app/assets/post.png",
+  "Sky diving was fun...",
+  "akhilboddu"
+);
+createPost(
+  "https://frabjous-pavlova-ebea4c.netlify.app/assets/post.png",
+  "iX is awesome",
+  "unam"
+);
+createPost(
+  "https://frabjous-pavlova-ebea4c.netlify.app/assets/post.png",
+  "Elon musk owns twitter now...",
+  "elonmusk"
+);
+createPost(
+  "https://frabjous-pavlova-ebea4c.netlify.app/assets/post.png",
+  "Demo object",
+  "demoperson"
+);
+createPost(
+  "https://frabjous-pavlova-ebea4c.netlify.app/assets/post.png",
+  "fruits are delicious",
+  "healthyperson"
+);
+
+updatePost(2, "www.imagelink.com/imagelink", "SpaceX next launch soon!");
+deletePost(4);
+deletePost(0);
